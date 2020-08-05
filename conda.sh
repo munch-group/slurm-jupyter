5b58162056ae6de5165b30370e50baab593336d1
Kasper Munch <kaspermunch@birc.au.dk>
2020-08-05 21:57:54 +0200

    Update conda.sh

--- conda.sh
+++ conda.sh
@@ -1,3 +1,16 @@
+
+# clean slate
+conda-build purge-all
+
+conda config --set anaconda_upload yes
+
+platform=`uname`
+if [ $platform == 'Darwin' ] ; then
+    plat='osx-64'
+else
+    plat='linux-64'
+fi
+
 # current dir (assume that is the package name)
 name=${PWD##*/}
 
@@ -10,14 +23,12 @@ for pythonversion in 3.6 3.7 3.8; do
 done
 
 # upload osx versions and convert to other architectures (assuming python only)
-for path in `find $HOME/anaconda3/conda-bld/osx-64/ -name "$name-[0-9]*.bz2"`; do
+for path in `find $CONDA_PREFIX/conda-bld/$plat/ -name "$name-[0-9]*.bz2"`; do
     anaconda upload $path
-    conda convert --platform all $HOME/anaconda3/conda-bld/osx-64/$name-[0-9]*.bz2 -o outputdir/
+    conda convert --platform all $path -o outputdir/
 done
 
 # upload versios for other architectures
 for path in `find outputdir -name '*.bz2'`; do 
     anaconda upload $path
 done
-
-
