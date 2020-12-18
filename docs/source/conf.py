133602b59fbc1ad0c76365ccdb86d9850b347709
Kasper Munch Terkelsen <kmt@ferb.lan.st.client.au.dk>
2020-12-18 08:59:42 +0100

    Typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Kasper Munch'
 author = 'Kasper Munch'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.6
+release = '2.0.6'
 
 
 # -- General configuration ---------------------------------------------------
