9e67e1be3e38eb7143eb232c30f5d6251928376a
Kasper Munch <kaspermunch@birc.au.dk>
2022-07-23 16:44:41 +0200

    bumped version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Kasper Munch'
 author = 'Kasper Munch'
 
 # The full version, including alpha/beta/rc tags
-release = '2.2.4'
+release = '2.2.5'
 
 
 # -- General configuration ---------------------------------------------------
