a8e2e889471eb6572deaa894e0d3a04ea5777193
Kasper Munch <kaspermunch@birc.au.dk>
2023-03-30 21:35:21 +0200

    Made miniconda default of both miniconda and anaconda exists

--- slurm_jupyter/templates.py
+++ slurm_jupyter/templates.py
@@ -37,23 +37,8 @@ slurm_server_script =  """#!/bin/sh
 {sources_loaded}
 ##cd "{cwd}"
 
-if [ -d "$HOME/anaconda3" ]
+if [ -d "$HOME/miniconda3" ]
 then
-    # >>> conda initialize >>>
-    # !! Contents within this block are managed by 'conda init' !!
-    __conda_setup="$('$HOME/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-    if [ $? -eq 0 ]; then
-        eval "$__conda_setup"
-    else
-        if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
-            . "$HOME/anaconda3/etc/profile.d/conda.sh"
-        else
-            export PATH="$HOME/anaconda3/bin:$PATH"
-        fi
-    fi
-    unset __conda_setup
-    # <<< conda initialize <<<
-else
     # >>> conda initialize >>>
     # !! Contents within this block are managed by 'conda init' !!
     __conda_setup="$('$HOME/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
@@ -64,6 +49,21 @@ else
             . "$HOME/miniconda3/etc/profile.d/conda.sh"
         else
             export PATH="$HOME/miniconda3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+    # <<< conda initialize <<<
+else
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$('$HOME/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+            . "$HOME/anaconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="$HOME/anaconda3/bin:$PATH"
         fi
     fi
     unset __conda_setup
