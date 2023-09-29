8026d6ad4aa2879dac9bcf6ee1d0432fa11821b1
Kasper Munch <kaspermunch@birc.au.dk>
2023-09-29 14:31:59 +0200

    Fixed support for mamba

--- slurm_jupyter/templates.py
+++ slurm_jupyter/templates.py
@@ -53,6 +53,26 @@ then
     fi
     unset __conda_setup
     # <<< conda initialize <<<
+elif  [ -d "$HOME/mambaforge" ]
+then
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$('$HOME/mambaforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "$HOME/mambaforge/etc/profile.d/conda.sh" ]; then
+            . "$HOME/mambaforge/etc/profile.d/conda.sh"
+        else
+            export PATH="$HOME/mambaforge/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+
+    if [ -f "$HOME/mambaforge/etc/profile.d/mamba.sh" ]; then
+        . "$HOME/mambaforge/etc/profile.d/mamba.sh"
+    fi
+    # <<< conda initialize <<<
 else
     # >>> conda initialize >>>
     # !! Contents within this block are managed by 'conda init' !!
