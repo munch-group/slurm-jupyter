d41ac5182ec6bee3764deaa23582b253ba0bf1b4
Kasper Munch <kmt@francis.eduroam.net.au.dk>
2023-11-27 13:16:39 +0100

    Added support for miniforge3

--- slurm_jupyter/templates.py
+++ slurm_jupyter/templates.py
@@ -73,6 +73,26 @@ then
         . "$HOME/mambaforge/etc/profile.d/mamba.sh"
     fi
     # <<< conda initialize <<<
+elif  [ -d "$HOME/miniforge3" ]
+then
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$('$HOME/miniforge3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "$HOME/miniforge3/etc/profile.d/conda.sh" ]; then
+            . "$HOME/miniforge3/etc/profile.d/conda.sh"
+        else
+            export PATH="$HOME/miniforge3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+
+    if [ -f "$HOME/miniforge3/etc/profile.d/mamba.sh" ]; then
+        . "$HOME/miniforge3/etc/profile.d/mamba.sh"
+    fi
+    # <<< conda initialize <<<
 else
     # >>> conda initialize >>>
     # !! Contents within this block are managed by 'conda init' !!
